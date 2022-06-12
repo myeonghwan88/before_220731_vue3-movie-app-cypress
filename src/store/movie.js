1eb9cc21161cefdbc66835a9d4afa434c1f3409d
myeonghwan88 <idmash88@gmail.com>
2022-06-12 18:43:59 +0900

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -75,11 +75,14 @@ export default {
             })
             const { Search } = res.data
             commit('updateState', {
-              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')] // ... : 전개연산자
+              movies: [
+                ...state.movies,
+                ..._uniqBy(Search, 'imdbID')
+              ] // ... : 전개연산자
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
           message
@@ -116,23 +119,6 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id
-  ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-  : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise ((resolve, reject) => {
-    axios.get(url)
-    .then(res => {
-      if (res.data.Error) {
-        reject(res.data.Error)
-      }
-      resolve(res)
-    })
-    .catch(err => {
-      reject(err.message)
-    })
-  })
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
