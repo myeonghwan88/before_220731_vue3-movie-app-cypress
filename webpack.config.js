88a6af0229e3a79bf1861a5445be9f8ed6deed96
myeonghwan88 <idmash88@gmail.com>
2022-06-12 18:59:30 +0900

    Create env

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const path = _require('path')
 const HtmlPlugin = _require('html-webpack-plugin')
 const CopyPlugin = _require('copy-webpack-plugin')
 const { VueLoaderPlugin } = _require('vue-loader')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   resolve: {
@@ -78,7 +79,8 @@ module.exports = {
         { from: 'static' }
       ]
     }),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new Dotenv()
   ],
 
   // 개발 서버 옵션
